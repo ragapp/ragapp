39c1b1a042de7535a3510257376b8d88fe8f73b3
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-06-12 11:26:06 +0000

    fix: remove patch files related to chat secion

--- patch/frontend/app/chat/page.tsx
+++ patch/frontend/app/chat/page.tsx
@@ -6,11 +6,9 @@
 import ChatSection from "../components/chat-section";
 
 export default function Home() {
-  return (
-    <main className="absolute w-full h-full flex flex-col items-center gap-10 bg-gray-50">
-      <div className="w-full h-full">
-        <ChatSection layout="fit" />
-      </div>
-    </main>
-  );
+	return (
+		<div className="h-full w-full">
+			<ChatSection />
+		</div>
+	);
 }
