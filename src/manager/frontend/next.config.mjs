5781e22e0b2bd48670ca40d414b22bda21f91819
leehuwuj <leehuwuj@gmail.com>
2024-07-26 10:59:09 +0700

    use sed to replace static

--- src/manager/frontend/next.config.mjs
+++ src/manager/frontend/next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: process.env.APP_PATH || '',
+    basePath: '',
     env: {
-        APP_PATH: process.env.APP_PATH || '',
         DEVELOPMENT: process.env.DEVELOPMENT,
     },
 };
