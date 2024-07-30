e7fcd45729fe1085545ae153b2b83e69a17f0dc6
leehuwuj <leehuwuj@gmail.com>
2024-07-30 16:15:17 +0700

    fix broken links for font and logo

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,18 @@
 
 if [[ -n "$BASE_URL" ]]; then
     # Run sed command for all files in the static directory
-    sed -i 's|"/_next/static|"'$BASE_URL'/_next/static|g' static/*.*
-    sed -i 's|"/admin/_next/static|"'$BASE_URL'/admin/_next/static|g' static/admin/*.*
-    sed -i 's|"/api/|"'$BASE_URL'/api/|g' static/*.*
+    # Replace this: url(/_next with this: url($BASE_URL/_next (Mostly for link in CSS files)
+    find static -type f -name "*.*" -exec sed -i 's|url(/_next|url('"$BASE_URL"'/_next|g' {} +
+    # Replace this: "static/ with this: "$BASE_URL/static/
+    # TODO: This is not working for some reason
+    find static -type f -name "*.*" -exec sed -i 's|"static/|"'$BASE_URL'/static/|g' {} +
+
+    # Logo and avatar
+    find static -type f -name "*.*" -exec sed -i 's|"/logo.png|"'$BASE_URL'/logo.png|g' {} +
+    find static -type f -name "*.*" -exec sed -i 's|"/llama.png|"'$BASE_URL'/llama.png|g' {} +
+
+    find static -type f -name "*.*" -exec sed -i 's|"/_next/static|"'$BASE_URL'/_next/static|g' {} +
+    find static/admin -type f -name "*.*" -exec sed -i 's|"/admin/_next/static|"'$BASE_URL'/admin/_next/static|g' {} +
 
     # Run sed command to generate window.ENV.API_URL to index.html
     # Add it at the start of the head tag
