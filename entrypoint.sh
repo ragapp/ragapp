1834632d801970877422799fe66ab0fde2229497
Marcus Schiesser <mail@marcusschiesser.de>
2024-07-30 20:00:08 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ if [[ -n "$BASE_URL" ]]; then
     find static -type f -name "*.*" -exec sed -i 's|"/logo.png|"'$BASE_URL'/logo.png|g' {} +
     find static -type f -name "*.*" -exec sed -i 's|"/llama.png|"'$BASE_URL'/llama.png|g' {} +
 
-    # Replacment for admin's static files
+    # Replace static files of admin app
     find static/admin -type f -name "*.*" -exec sed -i 's|"/admin/_next/static|"'$BASE_URL'/admin/_next/static|g' {} +
 
     # Run sed command to generate window.ENV.API_URL to index.html
