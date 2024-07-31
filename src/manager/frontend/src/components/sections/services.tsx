6cd3c76b8ced5a6aac789ba2731738db056ff7ad
leehuwuj <leehuwuj@gmail.com>
2024-07-31 10:11:58 +0700

    improve ui by tooltip

--- src/manager/frontend/src/components/sections/services.tsx
+++ src/manager/frontend/src/components/sections/services.tsx
@@ -3,13 +3,15 @@ import { Button } from "@/components/ui/button"
 import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card"
 import { getServices, startService, stopService, removeService } from "@/client/service";
 import { Service } from "@/client/models/service";
-import { TbRobot, TbFilePencil, TbPlayerPauseFilled, TbPlayerPlayFilled } from "react-icons/tb";
+import { TbFilePencil, TbPlayerPauseFilled, TbPlayerPlayFilled } from "react-icons/tb";
 import { ImSpinner8 } from "react-icons/im";
 import { TiDeleteOutline } from "react-icons/ti";
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "../ui/alert-dialog";
 
 import { useEffect, useState } from "react";
 import { CreateAgentDialog } from "./createAgent";
+import { TooltipProvider } from "@radix-ui/react-tooltip";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 
 function AlertDialogRemoveApp({
     service,
@@ -47,6 +49,50 @@ function AlertDialogRemoveApp({
     )
 }
 
+function StartStopButton({
+    service,
+    refetch
+}: {
+    service: Service,
+    refetch: () => void
+}) {
+    const [isHandling, setIsHandling] = useState(false);
+
+    async function handleStopService() {
+        setIsHandling(true);
+        await stopService(service.id);
+        refetch();
+        setIsHandling(false);
+    }
+
+    async function handleStartService() {
+        setIsHandling(true);
+        await startService(service.id);
+        refetch();
+        setIsHandling(false);
+    }
+
+    return (
+        service.status === "running"
+            ? (<Button
+                variant="outline"
+                className="flex items-center text-muted-foreground"
+                onClick={handleStopService}
+                disabled={isHandling}
+            >
+                <TbPlayerPauseFilled size={20} />
+            </Button>)
+            : (<Button
+                variant="outline"
+                className="flex items-center text-muted-foreground"
+                onClick={handleStartService}
+                disabled={isHandling}
+            >
+                <TbPlayerPlayFilled size={20} />
+            </ Button>)
+    )
+}
+
 function ServiceCard({
     service,
     refetch
@@ -54,67 +100,54 @@ function ServiceCard({
     service: Service,
     refetch: () => void
 }) {
-    async function handleStopService() {
-        await stopService(service.id);
-        refetch();
-    }
-
-    async function handleStartService() {
-        await startService(service.id);
-        refetch();
-    }
 
     return (
-        <Card key={service.id} className="p-4">
-            <CardHeader className="flex flex-row justify-between">
-                <CardTitle className="text-xl font-bold flex items-center text-foreground">
-                    <TbRobot size={30} />
-                    {service.app_name ? service.app_name : service.name}
-                </CardTitle>
-                <AlertDialogRemoveApp service={service} refetch={refetch} />
-            </CardHeader>
-            <CardContent>
-                <p className="font-bold text-foreground">State:</p>
-                <ul className="list-disc ml-5 text-muted-foreground">
-                    <li className="bold" >Status: {
-                        service.status === "running"
-                            ? <span className="text-green">
-                                Running</span>
-                            : <span className="text-red">Stopped</span>
-                    }</li>
-                    <li>Started At: {service.started_at}</li>
-                    <li>Restart Count: {service.restart_count}</li>
-                </ul>
-            </CardContent>
-            <CardFooter className="flex justify-between">
-                <a
-                    className="text-primary-foreground"
-                    href={`${service.url}/admin/`}
-                    target="_blank"
-                >
-                    <Button variant="outline" className="flex items-center text-muted-foreground">
-                        <TbFilePencil size={20} />
-                        Edit
-                    </Button>
-                </a>
-                {service.status === "running"
-                    ? (<Button
-                        variant="outline"
-                        className="flex items-center text-muted-foreground"
-                        onClick={handleStopService}
+        <TooltipProvider>
+            <Card key={service.id} className="shadow-md">
+                <CardHeader className="flex flex-row justify-between">
+                    <CardTitle className="text-xl font-bold flex items-center text-foreground">
+                        <a
+                            href={service.url}
+                            target="_blank"
+                        >
+                            {service.app_name ? service.app_name : service.name}
+                        </a>
+                    </CardTitle>
+                    <AlertDialogRemoveApp service={service} refetch={refetch} />
+                </CardHeader>
+                <CardContent>
+                    <p className="text-foreground">State: {" "}
+                        {
+                            service.status === "running"
+                                ? (
+                                    <Tooltip>
+                                        <TooltipTrigger>
+                                            <span className="text-green">Running</span>
+                                        </TooltipTrigger>
+                                        <TooltipContent side="right">
+                                            <span>since {service.started_at}</span>
+                                        </TooltipContent>
+                                    </Tooltip>
+                                )
+                                : <span className="text-red">Stopped</span>
+                        }
+                    </p>
+                </CardContent>
+                <CardFooter className="flex justify-between">
+                    <a
+                        className="text-primary-foreground"
+                        href={`${service.url}/admin/`}
+                        target="_blank"
                     >
-                        <TbPlayerPauseFilled size={20} />
-                    </Button>)
-                    : (<Button
-                        variant="outline"
-                        className="flex items-center text-muted-foreground"
-                        onClick={handleStartService}
-                    >
-                        <TbPlayerPlayFilled size={20} />
-                    </Button>)
-                }
-            </CardFooter>
-        </Card>
+                        <Button variant="outline" className="flex items-center text-muted-foreground">
+                            <TbFilePencil size={20} />
+                            Edit
+                        </Button>
+                    </a>
+                    <StartStopButton service={service} refetch={refetch} />
+                </CardFooter>
+            </Card>
+        </TooltipProvider>
     )
 }
 
