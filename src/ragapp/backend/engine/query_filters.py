b57d084b1161ac37398ddd6585b65dce01882246
leehuwuj <leehuwuj@gmail.com>
2024-09-13 17:53:08 +0700

    med
    tidier code

--- src/ragapp/backend/engine/query_filters.py
+++ src/ragapp/backend/engine/query_filters.py
@@ -1,4 +1,5 @@
 from app.engine.index import get_index
+from llama_index.core.vector_stores.types import MetadataFilter, MetadataFilters
 from llama_index.indices.managed.llama_cloud import LlamaCloudIndex
 
 
@@ -6,10 +7,37 @@ def generate_filters(doc_ids):
     current_index = get_index()
 
     if isinstance(current_index, LlamaCloudIndex):
-        from app.engine.llamacloud_filters import generate_filters
-
-        return generate_filters(doc_ids)
+        public_doc_filter = MetadataFilter(
+            key="private",
+            value=None,
+            operator="is_empty",  # type: ignore
+        )
     else:
-        from app.engine.query_filter import generate_filters
+        public_doc_filter = MetadataFilter(
+            key="private",
+            value="true",
+            operator="!=",  # type: ignore
+        )
 
-        return generate_filters(doc_ids)
+    selected_doc_filter = MetadataFilter(
+        key="file_id",  # Note: LLamaCloud uses "file_id" to reference private document ids as "doc_id" is a restricted field in LlamaCloud
+        value=doc_ids,
+        operator="in",  # type: ignore
+    )
+    if len(doc_ids) > 0:
+        # If doc_ids are provided, we will select both public and selected documents
+        filters = MetadataFilters(
+            filters=[
+                public_doc_filter,
+                selected_doc_filter,
+            ],
+            condition="or",  # type: ignore
+        )
+    else:
+        filters = MetadataFilters(
+            filters=[
+                public_doc_filter,
+            ]
+        )
+
+    return filters
