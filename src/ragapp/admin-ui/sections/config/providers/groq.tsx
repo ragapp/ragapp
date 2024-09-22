9c9d98347c18ef2936b829d83ef30559f8690641
leehuwuj <leehuwuj@gmail.com>
2024-09-22 10:25:55 +0700

    Fix groq model issue

--- src/ragapp/admin-ui/sections/config/providers/groq.tsx
+++ src/ragapp/admin-ui/sections/config/providers/groq.tsx
@@ -17,7 +17,22 @@ export const GroqForm = ({
   form: UseFormReturn;
   defaultValues: any;
 }) => {
-  const supportingModels = ["llama3-8b", "llama3-70b", "mixtral-8x7b"];
+  const supportingModels = [
+    // Llama
+    "llam`a-3.1-70b-versatile",
+    "llama-3.1-8b-instant",
+    "llama3-8b-8192",
+    "llama3-70b-8192",
+    "llama3-groq-70b-8192-tool-use-preview",
+    "llama3-groq-8b-8192-tool-use-preview",
+    // Mixtral
+    "mixtral-8x7b-32768",
+    // Gemma
+    "gemma2-9b-it",
+    "gemma-7b-it",
+    // LLaVA
+    "llava-v1.5-7b-4096-preview",
+  ];
 
   return (
     <>
