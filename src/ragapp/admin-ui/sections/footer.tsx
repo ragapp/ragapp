f84610e733e5c3297b2fae6781113150896d3f51
Moe <moe@digilite.ca>
2024-10-08 13:07:10 -0400

    Add RayaTech

--- src/ragapp/admin-ui/sections/footer.tsx
+++ src/ragapp/admin-ui/sections/footer.tsx
@@ -1,16 +1,7 @@
 const Footer = () => {
   return (
     <div className="p-4 text-center bg-gray-200 w-full">
-      <p>
-        Questions, feature requests or found a bug? Open an issue on&nbsp;
-        <a
-          href="https://github.com/ragapp/ragapp/issues/"
-          className="text-blue-500 hover:underline decoration-blue-500"
-        >
-          GitHub
-        </a>
-        .&nbsp;© 2024 by Schiesser IT, LLC.
-      </p>
+      <p>© 2024 by RayaTech.</p>
     </div>
   );
 };
