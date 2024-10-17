f20961f0107d37403eecc804f9d854ccc0ab923c
Moe <moe@digilite.ca>
2024-10-17 13:06:35 -0400

    rebrand

--- src/ragapp/admin-ui/sections/footer.tsx
+++ src/ragapp/admin-ui/sections/footer.tsx
@@ -1,18 +1,5 @@
 const Footer = () => {
-  return (
-    <div className="p-4 text-center bg-gray-200 w-full">
-      <p>
-        Questions, feature requests or found a bug? Open an issue on&nbsp;
-        <a
-          href="https://github.com/ragapp/ragapp/issues/"
-          className="text-blue-500 hover:underline decoration-blue-500"
-        >
-          GitHub
-        </a>
-        .&nbsp;© 2024 by Schiesser IT, LLC.
-      </p>
-    </div>
-  );
+  return <div className="p-4 text-center bg-gray-200 w-full"></div>;
 };
 
 export { Footer };
