89d514f7ef5af2b8e5ce36544d90c6f0a86f03d4
leehuwuj <leehuwuj@gmail.com>
2024-10-24 16:21:33 +0700

    refine prompt

--- src/ragapp/backend/models/tools/document_generator.py
+++ src/ragapp/backend/models/tools/document_generator.py
@@ -1,3 +1,4 @@
+from textwrap import dedent
 from typing import ClassVar, Dict, Literal, Optional
 
 from pydantic import BaseModel
@@ -9,9 +10,14 @@ class DocumentGeneratorTool(BaseModel):
     tool_type: Literal["local"] = "local"
     label: Literal["Document Generator"] = "Document Generator"
     description: str = "Generate a document file (PDF, HTML)"
-    custom_prompt: Optional[
-        str
-    ] = """If user request for a report or a post, use document generator tool to create a file and reply with the link to the file.
-        The output link of the generated file must start with '/api/files/output/tool/<file>'. It must be a relative path, don't try adding the domain name or protocol"""
+    custom_prompt: Optional[str] = dedent(
+        """
+        If user request for a report or a post, use document generator tool to create a file and reply with the link to the file.
+        
+        IMPORTANT:
+        - Always remove the "sandbox:" prefix from a link before generating a file.
+        - The output link of the generated file must start with '/api/files/output/tool/<file>'. It must be a relative path; do not add the domain name or protocol.
+        """
+    )
     config: Dict = {}
     enabled: bool = False
