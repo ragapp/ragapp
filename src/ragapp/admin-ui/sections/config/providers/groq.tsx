f1f60aed84f29d8327a4b3c8d1f9db795ab338a6
leehuwuj <leehuwuj@gmail.com>
2024-10-31 08:34:48 +0700

    fix wrong groq model name

--- src/ragapp/admin-ui/sections/config/providers/groq.tsx
+++ src/ragapp/admin-ui/sections/config/providers/groq.tsx
@@ -19,7 +19,7 @@ export const GroqForm = ({
 }) => {
   const supportingModels = [
     // Llama
-    "llam`a-3.1-70b-versatile",
+    "llama-3.1-70b-versatile",
     "llama-3.1-8b-instant",
     "llama3-8b-8192",
     "llama3-70b-8192",
