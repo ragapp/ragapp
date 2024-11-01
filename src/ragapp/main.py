0f69eded1df45d00ad3269d20da88e8ea90566e8
leehuwuj <leehuwuj@gmail.com>
2024-11-01 08:42:07 +0700

    add missing backend patching

--- src/ragapp/main.py
+++ src/ragapp/main.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 from backend.constants import ENV_FILE_PATH
 
-load_dotenv(dotenv_path=ENV_FILE_PATH, verbose=False, override=True)
+load_dotenv(dotenv_path=ENV_FILE_PATH, verbose=False, override=False)
 
 # flake8: noqa
 import logging
